9f63ce8f785e54e639d7b63466974aa8f4cd926a
zizoumunoz <zmunoz@outlook.es>
2020-09-30 15:02:04 -0700

    Added "your mad lib is:" line.

--- Madlibs/src/Madlibs.java
+++ Madlibs/src/Madlibs.java
@@ -1,21 +1,21 @@
-import java.util.Scanner; 
+import java.util.Scanner;
 
 public class Madlibs {
 
 	public static void main(String[] args) {
 		// makes a new Scanner called 'keyScan'
 		Scanner keyScan = new Scanner(System.in);
-		
+
 		String color = "__";
 		String verb = "__";
 		String adjective = "__";
-		
+
 		// composes the madLib template
 		String madLib = "The " + color + " dragon " + verb + " at the " + adjective + " knight.";
-		
+
 		// prints the mad lib template
 		System.out.println(madLib);
-		
+
 		// asks user for a color, past tense verb, and an adjective to put in the madlib
 		System.out.println("Enter a color:");
 		color = keyScan.nextLine();
@@ -23,15 +23,16 @@ public class Madlibs {
 		verb = keyScan.nextLine();
 		System.out.println("Enter an adjective");
 		adjective = keyScan.nextLine();
-		
+
 		// composes the madlib again
 		madLib = "The " + color + " dragon " + verb + " at the " + adjective + " knight.";
-		
+
 		// prints madlib
+		System.out.println("Your MadLib is: ");
 		System.out.println(madLib);
-		
+
 		// closes scanner
 		keyScan.close();
 	}
-
+	
 }
