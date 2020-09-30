80b21f9d40eb2ac3c92c97b0d8c18f005522d896
zizoumunoz <zmunoz@outlook.es>
2020-09-30 14:49:16 -0700

    Created main madLib string.

--- Madlibs/src/Madlibs.java
+++ Madlibs/src/Madlibs.java
@@ -3,8 +3,15 @@ import java.util.Scanner;
 public class Madlibs {
 
 	public static void main(String[] args) {
+		Scanner keyScan = new Scanner(System.in);
+		String color = "__";
+		String verb = "__";
+		String adjective = "__";
+		String madLib = "The " + color + " dragon " + verb + " at the " + adjective + " knight.";
 		
-
+		System.out.println(madLib);
+		
+		keyScan.close();
 	}
 
 }
